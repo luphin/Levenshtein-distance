0a6b03e9544923822a5511155be32d9b3b19ec2f
luphin <sebaszegarra@gmail.com>
2024-11-15 05:40:39 -0300

    update report and Dir cogidos

--- codigos/graficos.py
+++ codigos/graficos.py
@@ -34,17 +34,19 @@ promedios_largos_ordenados, tiemposFB_ordenados, memoriasFB_ordenadas, tiemposDP
 plt.figure(figsize=(12, 6))
 plt.plot(promedios_largos_ordenados, tiemposFB_ordenados, label='Tiempo Fuerza Bruta', marker='o', color='red')
 plt.plot(promedios_largos_ordenados, tiemposDP_ordenados, label='Tiempo Programación Dinámica', marker='x', color='green')
+# Labels
 plt.xlabel('Promedio del Largo de los Strings en Cada Caso')
 plt.ylabel('Tiempo de Ejecución (microsegundos)')
 plt.yscale('log')  # Aplicar escala logarítmica en el eje Y
 plt.legend()
+# Title
 plt.title('Tiempo de Ejecución vs Promedio de Largo de los Strings (Escala Logarítmica)')
 
 # Añadir etiquetas para cada punto, alternando la posición
 for i, (promedio, tiempo_fb, tiempo_dp) in enumerate(zip(promedios_largos_ordenados, tiemposFB_ordenados, tiemposDP_ordenados)):
-    offset = (0, 10) if i % 2 == 0 else (0, -15)  # Alternar posiciones
+    offset = (0, 10) if i % 2 == 0 else (0, -15)  # Alternar posiciones FB
     plt.annotate(f"{tiempo_fb}", (promedio, tiempo_fb), textcoords="offset points", xytext=offset, ha='center', fontsize=8, color='red')
-    offset_dp = (0, 10) if i % 2 != 0 else (0, -15)
+    offset_dp = (0, 10) if i % 2 != 0 else (0, -15) # Alternar posiciones PD
     plt.annotate(f"{tiempo_dp}", (promedio, tiempo_dp), textcoords="offset points", xytext=offset_dp, ha='center', fontsize=8, color='green')
 
 plt.show()
@@ -53,16 +55,18 @@ plt.show()
 plt.figure(figsize=(12, 6))
 plt.plot(promedios_largos_ordenados, memoriasFB_ordenadas, label='Memoria Fuerza Bruta', marker='o', color='red')
 plt.plot(promedios_largos_ordenados, memoriasDP_ordenadas, label='Memoria Programación Dinámica', marker='x', color='green')
+# Labels
 plt.xlabel('Promedio del Largo de los Strings en Cada Caso')
 plt.ylabel('Memoria (KB)')
 plt.legend()
+# Title
 plt.title('Memoria vs Promedio de Largo de los Strings')
 
 # Añadir etiquetas para cada punto, alternando la posición
 for i, (promedio, memoria_fb, memoria_dp) in enumerate(zip(promedios_largos_ordenados, memoriasFB_ordenadas, memoriasDP_ordenadas)):
-    offset = (0, 10) if i % 2 == 0 else (0, -15)  # Alternar posiciones
+    offset = (0, 10) if i % 2 == 0 else (0, -15)  # Alternar posiciones FB
     plt.annotate(f"{memoria_fb}", (promedio, memoria_fb), textcoords="offset points", xytext=offset, ha='center', fontsize=8, color='red')
-    offset_dp = (0, 10) if i % 2 != 0 else (0, -15)
+    offset_dp = (0, 10) if i % 2 != 0 else (0, -15) # Alternar posiciones PD
     plt.annotate(f"{memoria_dp}", (promedio, memoria_dp), textcoords="offset points", xytext=offset_dp, ha='center', fontsize=8, color='green')
 
 plt.show()
